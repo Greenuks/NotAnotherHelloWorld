8f1dd9de4842eaa6cf4c7cd17818fbc2e1d98e1c
Greenuks <zielu@protonmail.com>
2018-04-22 08:18:05 +0200

    Chapter 1

--- First app/run.py
+++ First app/run.py
@@ -1,6 +1,7 @@
 print("Hello word")
-print("1,2,3")
-print("4,5,6,7")
-print("0000000000")
-print(1,2,4)
-print
+zmienna='Hello'
+print(zmienna)
+print('maciej')
+to_jest_dluga_zmienna=''
+print(to_jest_dluga_zmienna)
+print((2+3)*6)
